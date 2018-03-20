10c262169cd860e8ae865fdd09898a8bb12d3649
Thomas Crone Larsen <thom802b@edu.eal.dk>
2018-03-20 16:44:03 +0100

    Initial commit

---
+++ YatzyApp/Classes/DiceHandler.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace YatzyApp.Classes
+{
+    class DiceHandler
+    {
+        private List<Dice> DiceList;
+        protected Random rnd = new Random();
+        public DiceHandler()
+        {
+            DiceList = new List<Dice>() { new Dice(rnd), new Dice(rnd), new Dice(rnd), new Dice(rnd), new Dice(rnd), new Dice(rnd) };
+        }
+        public void HoldDice(int index)
+        {
+            if (DiceList.Count < index || index < 0)
+                throw new Exception("Dice index not in list - index less than zero or greater than 5");
+
+            if (DiceList[index].Held)
+                DiceList[index].Held = false;
+            else
+                DiceList[index].Held = true;
+        }
+        public List<Dice> RollDices()
+        {
+            foreach (Dice d in DiceList)
+            {
+                d.Roll();
+            }
+            return DiceList;
+        }
+    }
+}
---
+++ YatzyApp/MainActivity.cs
@@ -0,0 +1,34 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using YatzyApp.Classes;
+
+namespace YatzyApp
+{
+    [Activity(Label = "YatzyApp", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        DiceHandler diceH = new DiceHandler();
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+            Button button = FindViewById<Button>(Resource.Id.MyButton);
+            var label = FindViewById<TextView>(Resource.Id.Result);
+
+            button.Click += delegate
+            {
+                label.Text = "You rolled";
+                foreach (Dice d in diceH.RollDices())
+                {
+                    label.Text = label.Text + " - " + d.CurrentValue;
+                }
+                //button.Text = string.Format("{0} Clicks!", count++);
+                //label.Text = string.Format("{0} Clicks!", count++);
+            };
+        }
+    }
+}
+
